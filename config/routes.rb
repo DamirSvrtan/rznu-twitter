0fda4adbc494b7481e19b22717b1ec6383dfa2c0
damir <damir.svrtan@gmail.com>
2013-11-22 23:09:17 +0100

    Reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 RznuTwitter::Application.routes.draw do
 
-  resources :users do
+  resources :users, only: [:show, :new, :index] do
     resources :tweets
   end
-  root 'home#index'
-  get '/routes', to: 'routes#index'
 
-  resources :sessions, only: [:new, :create, :destroy]
   get '/signup',  to: 'users#new'  
+  resources :sessions, only: [:create]
   get '/signin',  to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy', via: :delete
+  root 'home#index'
+  get '/routes', to: 'routes#index'
 end
