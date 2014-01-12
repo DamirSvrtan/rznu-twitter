d2a598678dba2c145e1fba8c3a173b213d45cee3
damir <damir.svrtan@gmail.com>
2014-01-12 22:42:13 +0100

    Better Hadoop Readme notes

--- app/controllers/api/stats_controller.rb
+++ app/controllers/api/stats_controller.rb
@@ -1,7 +1,7 @@
 class Api::StatsController < ApplicationController
 
   def popular
-    @occurrences = File.readlines('/tmp/rznu-logs-map-reduce/browser-output-2')
+    @occurrences = File.readlines('/tmp/rznu-logs-map-reduce/browser-output')
   end
 
   def how_to
